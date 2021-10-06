0f06a77533f15400ff3162a8f932c36a48d9a121
fjij <30779570+fjij@users.noreply.github.com>
2021-10-06 01:53:40 -0400

    Deployment infrastructure

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -26,9 +26,9 @@ const config: HardhatUserConfig = {
   solidity: "0.8.4",
   networks: {
     ropsten: {
-      url: process.env.ROPSTEN_URL || "",
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+      url: process.env.ROPSTEN_URL,
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+      chainId: 3,
     },
   },
   gasReporter: {
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -3,54 +3,28 @@
 //
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
-import { ethers } from "hardhat";
-import { NFTStorage, File } from 'nft.storage';
+import hre, { ethers } from 'hardhat';
+import CardLocation from '../cards/location.json';
 import fs from 'fs-extra';
 import path from 'path';
 
-const apiKey = process.env.NFT_STORAGE_API_KEY;
-if (!apiKey) {
-  throw new Error('Missing environment variable NFT_STORAGE_API_KEY');
-}
-const client = new NFTStorage({ token: apiKey });
-
-function readDirectory(dir: string): File[] {
-  const names = fs.readdirSync(dir);
-  return names.map(name => 
-    new File([fs.readFileSync(path.join(dir, name)) as BlobPart], name));
-}
-
-async function uploadCards(): Promise<[string, number]> {
-  const images = readDirectory(path.join(__dirname, '../cards/images'));
-  const imageMetadata = await client.storeDirectory(images);
-  console.log("Images uploaded to: ", imageMetadata);
-  const cardFiles = readDirectory(path.join(__dirname, '../cards/data'));
-  const cardFilesModified = await Promise.all(cardFiles.map(async file => {
-    const obj = JSON.parse(await file.text());
-    obj.image = `ipfs://${imageMetadata.toString()}/${obj.image}`;
-    return new File([JSON.stringify(obj)], file.name);
-  }));
-  const fileMetadata = await client.storeDirectory(cardFilesModified);
-  console.log("Files uploaded to: ", fileMetadata);
-  return [fileMetadata.toString(), cardFiles.length];
-}
-
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
-  
-  const [hash, cardCount] = await uploadCards();
+  const { metadata, count } = CardLocation;
 
-  const Cards = await ethers.getContractFactory("Cards");
-  const cards = await Cards.deploy(`ipfs://${hash}/{id}.json`, cardCount);
+  const Cards = await ethers.getContractFactory('Cards');
+  const cards = await Cards.deploy(`ipfs://${metadata}/{id}.json`, count);
 
   await cards.deployed();
 
-  console.log("Cards deployed to:", cards.address);
+  console.log('Cards deployed to:', cards.address);
+
+  const deploymentDir = path.join(__dirname, `../deployments/${hre.network.name}`);
+  await fs.mkdirp(deploymentDir);
+  fs.writeFileSync(path.join(deploymentDir, 'deployment.json'), JSON.stringify({
+    address: cards.address,
+    chainId: hre.network.config.chainId,
+    url: (hre.network.config as any).url,
+  }, null, 2));
 }
 
 // We recommend this pattern to be able to use async/await everywhere
