e570736486cbff53e66297693d782963f6ba2e4e
kaStinju <40683050+kaStinju@users.noreply.github.com>
2021-10-08 01:54:34 -0400

    changed id to key

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,44 +1,37 @@
-import React from 'react';
-import Matchmaking from './Matchmaking';
-import ConnectWallet from './ConnectWallet';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 //pages
-import Home from './Home';
-import MyCollection from './MyCollection';
-import Store from './Store';
-import Play from './Play'
+import Home from "./Home";
+import MyCollection from "./MyCollection";
+import Store from "./Store";
+import Play from "./Play";
 
 function App() {
   return (
     <>
-    <Router>
-      <Switch>
-      <div className="app">
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/myCollection">
-          <MyCollection/>
-        </Route>
-        <Route path="/store">
-          <Store/>
-        </Route>
-        <Route path="/play">
-          <Play />
-        </Route>
-      </div>
-      </Switch>
-    </Router>
+      <Router>
+        <Switch>
+          <div className="app">
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/myCollection">
+              <MyCollection />
+            </Route>
+            <Route path="/store">
+              <Store />
+            </Route>
+            <Route path="/play">
+              <Play />
+            </Route>
+          </div>
+        </Switch>
+      </Router>
     </>
-  )
+  );
 }
 
 export default App;
--- web/src/Home.tsx
+++ web/src/Home.tsx
@@ -1,66 +1,66 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Home.css';
-import Matchmaking from './Matchmaking';
-import ConnectWallet from './ConnectWallet';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Home.css";
+import Matchmaking from "./Matchmaking";
+import ConnectWallet from "./ConnectWallet";
+import { contract } from "./eth";
 
 interface HomeCard {
-  id: number;
+  key: number;
   img: string;
   text: string;
   link: string;
 }
 
 export default function Home() {
-    return (
-        <>
-        <div className="headings">
+  return (
+    <>
+      <div className="headings">
         <h1 className="title">Super Card Game</h1>
-        <ConnectWallet/>
-        </div>
-        <div className="links">
-        {homeInfo.map((card)=>{
-            return <CreateHomeCard {...card}></CreateHomeCard>;
+        <ConnectWallet />
+      </div>
+      <div className="links">
+        {homeInfo.map((card) => {
+          return <CreateHomeCard {...card}></CreateHomeCard>;
         })}
-        </div>
-        </>
-    )
+      </div>
+    </>
+  );
 }
 
 const CreateHomeCard = (props: HomeCard) => {
-    const {id, img, text, link} = props
-    return (
-        <>
-        <Link className="text" to={link}>
+  const { img, text, link } = props;
+  return (
+    <>
+      <Link className="text" to={link}>
         <article className="card">
-            <section className="center">
-                <img src={img}/>
-            </section>
-            <p className="spacing">{text}</p>
+          <section className="center">
+            <img src={img} />
+          </section>
+          <p className="spacing">{text}</p>
         </article>
-        </Link>
-        </>
-    )
-}
+      </Link>
+    </>
+  );
+};
 
-const homeInfo = 
-[
-    {
-        id: 1,
-        img: "https://ipfs.io/ipfs/bafybeigrvkhrn2egu4yhjivmbd2yxqnojlbaiatfwlquuxwymi4jfhe4fu/2-roe-creature.png",
-        text: "Play",
-        link: "/play"
-    },
-    {
-        id: 2,
-        img: "https://ipfs.io/ipfs/bafybeigrvkhrn2egu4yhjivmbd2yxqnojlbaiatfwlquuxwymi4jfhe4fu/2-roe-creature.png",
-        text: "Collection",
-        link: "/myCollection"
-    },
-    {
-        id: 3,
-        img: "https://ipfs.io/ipfs/bafybeigrvkhrn2egu4yhjivmbd2yxqnojlbaiatfwlquuxwymi4jfhe4fu/2-roe-creature.png",
-        text: "Store",
-        link: "/store"
-    },
-]
+const homeInfo = [
+  {
+    key: 1,
+    img: "https://ipfs.io/ipfs/bafybeigrvkhrn2egu4yhjivmbd2yxqnojlbaiatfwlquuxwymi4jfhe4fu/2-roe-creature.png",
+    text: "Play",
+    link: "/play",
+  },
+  {
+    key: 2,
+    img: "https://ipfs.io/ipfs/bafybeigrvkhrn2egu4yhjivmbd2yxqnojlbaiatfwlquuxwymi4jfhe4fu/2-roe-creature.png",
+    text: "Collection",
+    link: "/myCollection",
+  },
+  {
+    key: 3,
+    img: "https://ipfs.io/ipfs/bafybeigrvkhrn2egu4yhjivmbd2yxqnojlbaiatfwlquuxwymi4jfhe4fu/2-roe-creature.png",
+    text: "Store",
+    link: "/store",
+  },
+];
