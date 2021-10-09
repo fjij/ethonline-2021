2f2e5443c8036bce4449c35ef9694d126d61c059
kaStinju <40683050+kaStinju@users.noreply.github.com>
2021-10-08 21:42:27 -0400

    playing board

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,48 +1,46 @@
-import React, { useState } from 'react';
-import './styles/App.css';
+import React, { useState } from "react";
+import "./styles/App.css";
 
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 //pages
-import Home from './components/Home';
-import MyCollection from './components/MyCollection';
-import Store from './components/Store';
-import Play from './components/Play'
-import ConnectWallet from './components/ConnectWallet';
+import Home from "./components/Home";
+import MyCollection from "./components/MyCollection";
+import Store from "./components/Store";
+import Play from "./components/Play";
+import ConnectWallet from "./components/ConnectWallet";
+import Game from "./components/Game";
 
-import Navbar from './components/Navbar';
+import Navbar from "./components/Navbar";
 
 function App() {
-
   const [isConnected, setConnected] = useState(false);
   return (
     <div className="app">
       <Router>
         <Navbar />
-        { !isConnected &&
-          <ConnectWallet
-            onConnected={() => setConnected(true)}
-          />
-        }
-        { isConnected &&
+        {!isConnected && (
+          <ConnectWallet onConnected={() => setConnected(true)} />
+        )}
+        {isConnected && (
           <Switch>
             <Route exact path="/">
               <Home />
             </Route>
             <Route path="/myCollection">
-              <MyCollection/>
+              <MyCollection />
             </Route>
             <Route path="/store">
-              <Store/>
+              <Store />
             </Route>
             <Route path="/play">
-              <Play />
+              <Game />
             </Route>
           </Switch>
-        }
+        )}
       </Router>
     </div>
-  )
+  );
 }
 
 export default App;
---
+++ web/src/components/Game.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import "../styles/Game.css";
+
+export default function Game() {
+  return (
+    <>
+      <div className="layout">
+        <div className="player2">
+          <div className="player2used">
+            <div className="player2discard">
+              <div className="box"></div>
+            </div>
+            <div className="box"></div>
+          </div>
+          <div className="player2hand">
+            <div className="player2bench">
+              <div className="box"></div>
+              <div className="box"></div>
+              <div className="box"></div>
+              <div className="box"></div>
+              <div className="player2back">
+                <div className="box"></div>
+              </div>
+            </div>
+          </div>
+          <div className="player2deck">
+            <div className="box"></div>
+          </div>
+        </div>
+        <div className="player-play">
+          <div className="left-player">
+            <div className="box-play"></div>
+          </div>
+          <div className="box-play"></div>
+        </div>
+        <div className="player2">
+          <div className="player2used">
+            <div className="player2discard">
+              <div className="box"></div>
+            </div>
+            <div className="box"></div>
+          </div>
+          <div className="player2hand">
+            <div className="player2bench">
+              <div className="box"></div>
+              <div className="box"></div>
+              <div className="box"></div>
+              <div className="box"></div>
+              <div className="player2back">
+                <div className="box"></div>
+              </div>
+            </div>
+          </div>
+          <div className="player2deck">
+            <div className="box"></div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
