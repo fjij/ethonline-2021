9cad51795d1beb3cb78dbf684ff4e91929c436db
kaStinju <40683050+kaStinju@users.noreply.github.com>
2021-10-10 03:53:28 -0400

    store update

--- web/src/components/Store.tsx
+++ web/src/components/Store.tsx
@@ -1,9 +1,50 @@
-import React from 'react';
+import React from "react";
+import "../styles/Store.css";
+import banner0 from "../assets/oroeginsBanner.png";
+import placeholderbanner from "../assets/placeholderbanner.png";
+
+interface StoreBannerProps {
+  img: string;
+  name: string;
+}
 
 export default function Store() {
-    return (
-        <>
-        <h1>Store</h1>
-        </>
-    )
+  return (
+    <>
+      <div className="heading"></div>
+      <div className="banners">
+        {banners.map((banner) => {
+          return <StoreBanner {...banner}></StoreBanner>;
+        })}
+      </div>
+    </>
+  );
 }
+
+const StoreBanner = (props: StoreBannerProps) => {
+  const { img, name } = props;
+  return (
+    <article className="set-placeholder">
+      <img className={name} src={img}></img>
+    </article>
+  );
+};
+
+const banners = [
+  {
+    img: banner0,
+    name: "oroegins",
+  },
+  {
+    img: placeholderbanner,
+    name: "set-placeholder",
+  },
+  {
+    img: placeholderbanner,
+    name: "set-placeholder",
+  },
+  {
+    img: placeholderbanner,
+    name: "set-placeholder",
+  },
+];
